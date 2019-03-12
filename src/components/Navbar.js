9ec7392fac9186499ec413eb53b0f3f0738ca368
aleku399 <aleku399@gmail.com>
2019-03-12 15:54:38 +0300

    feature: add  navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,72 +1,62 @@
 import React from "react";
 import Link from "gatsby-link";
-import {
-Collapse,
-Navbar,
-NavbarBrand,
-NavbarToggler,
-Nav,
-NavItem
-} from "reactstrap";
 
 const Navlayout = class extends React.Component {
   constructor(props) {
     super(props);
-
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.closeNavbar = this.closeNavbar.bind(this);
-    this.state = {
-    collapsed: true
-    };
   }
 
-  toggleNavbar() {
-    this.setState({
-    collapsed: !this.state.collapsed
-    });
-  }
-
-  closeNavbar() {
-    if (this.state.collapsed !== true) {
-    this.toggleNavbar();
+  componentDidMount(){
+    function classToggle() {
+      const navs = document.querySelectorAll('.Navbar__Items')
+      
+      navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
     }
+    document.querySelector('.Navbar__Link-toggle')
+    .addEventListener('click', classToggle);
   }
 
  render() {
    return (
   
-  <Navbar  className="navigation__navbar fixed-top navbar navbar-expand-md bg-white" color="white" expand="md">
-    <NavbarBrand>
+  <div className="Navbar fixed-top">
+    <div className="Navbar__Link Navbar__Link-brand">
       <Link className="navbar-brand" to="/"  activeStyle={{ color: "black" }} title="bettinah">
           BETTINAH TIANAH
       </Link>
-    </NavbarBrand>
-    <NavbarToggler className="custom-toggler" onClick={this.toggleNavbar} />
-    <Collapse isOpen={!this.state.collapsed} navbar>
-      <Nav className="ml-auto" navbar>
-          <NavItem>
-            <Link className="nav-link navigation__navlinks"  onClick={this.closeNavbar}  activeStyle={{ color: "black" }}  to="/about">
-                ABOUT ME
-            </Link>
-          </NavItem>
-          <NavItem>
-            <Link className="nav-link navigation__navlinks"  onClick={this.closeNavbar}  activeStyle={{ color: "black" }} to="/contact">
-                CONTACT
-              </Link>
-          </NavItem>
-          <NavItem>
-            <Link className="nav-link navigation__navlinks"  onClick={this.closeNavbar} activeStyle={{ color: "black" }} to="/portfolio">
-                PORTFOLIO
-            </Link>
-          </NavItem>
-          <NavItem>
-            <Link className="nav-link navigation__navlinks"  onClick={this.closeNavbar}  activeStyle={{ color: "black" }} to="/news">
-                BETTINAHTIANAH NEWS
-            </Link>
-          </NavItem>
-      </Nav>
-    </Collapse>
-  </Navbar>
+    </div>
+    <div
+      className={`Navbar__Link Navbar__Link-toggle`}
+      data-target="navMenu" 
+    >
+       <i className="fas fa-bars"></i>
+    </div>
+    <nav
+      id="navMenu"
+      className={`Navbar__Items Navbar__Items--right `}
+    >
+      <div className="Navbar__Link">
+        <Link className="nav-link navigation__navlinks"  onClick={this.closeNavbar}  activeStyle={{ color: "black" }}  to="/about">
+          ABOUT ME
+        </Link>
+      </div>
+      <div className="Navbar__Link">
+        <Link className="nav-link navigation__navlinks"  onClick={this.closeNavbar}  activeStyle={{ color: "black" }} to="/contact">
+          CONTACT
+        </Link>
+      </div>
+      <div className="Navbar__Link">
+        <Link className="nav-link navigation__navlinks"  onClick={this.closeNavbar} activeStyle={{ color: "black" }} to="/portfolio">
+          PORTFOLIO
+        </Link>
+      </div>
+      <div className="Navbar__Link">
+        <Link className="nav-link navigation__navlinks"  onClick={this.closeNavbar}  activeStyle={{ color: "black" }} to="/news">
+          BETTINAHTIANAH NEWS
+        </Link>
+      </div>
+    </nav>
+  </div>
   )}
 }
 
