48789b0ba501f54c280437a32f306b994215c914
aleku399 <aleku399@gmail.com>
2019-03-15 10:41:29 +0300

    fix: css breaking

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,4 +2,3 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import 'bootstrap-css-only/css/bootstrap.min.css'
 import 'mdbreact/dist/css/mdb.css'
-
