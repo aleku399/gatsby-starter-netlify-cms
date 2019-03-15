7c1a60377e428415d8166bc67ecd8fa3e4dd5cbd
aleku399 <aleku399@gmail.com>
2019-03-15 20:38:22 +0300

    fix: adding css from remark images

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,8 +69,8 @@ module.exports = {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 750,
-              maxHeight: 900,
+              maxWidth: 350,
+              maxHeight: 200,
               quality: 100,
             },
           },
